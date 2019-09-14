ad09cc61829d460ef292fc087391bf1d49bbc64b
Tatiana Kurochkina <tatianakurochkina96@mail.ru>
2019-09-15 00:00:38 +0400

    Add sections

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
+import './styles/main.pcss';
+if (process.env.NODE_ENV === 'development') {
+  require('file-loader!./index.pug');
 }
 
-import "./scripts/skills";
+import './scripts/parallax';
