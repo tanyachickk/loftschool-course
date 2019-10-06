879147d60bd6963e1630d5bf4b0c4e9224788bfd
Tatiana Kurochkina <tatianakurochkina96@mail.ru>
2019-10-06 17:31:21 +0400

    show mobile menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,12 +1,40 @@
 const closedClass = 'hamburger_closed';
 const menu = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+const anchors = document.querySelectorAll('a[href*="#"]');
 
-function onHamburgerClick() {
-  if (menu.classList.contains(closedClass)) {
-      menu.classList.remove(closedClass);
+function closeMenu() {
+  document.body.style.overflow = 'auto';
+  menu.classList.remove(closedClass);
+}
+
+function openMenu() {
+  document.body.style.overflow = 'hidden';
+  menu.classList.add(closedClass);
+}
+
+function isMenuOpened() {
+  return menu.classList.contains(closedClass);
+}
+
+function onHamburgerClick(event) {
+  event.stopPropagation();
+  if (isMenuOpened()) {
+    closeMenu();
   } else {
-      menu.classList.add(closedClass);
+    openMenu();
   }
 }
 
+document.addEventListener('click', (event) => {
+  const isClickInside = sidebar.contains(event.target);
+  if (isMenuOpened() && !isClickInside) {
+    closeMenu();
+  }
+});
+
 menu.addEventListener('click', onHamburgerClick);
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', closeMenu);
+}
