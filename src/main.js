ac2ac00e43f6002c4978c2e4795a5df00fa3958f
Tatiana Kurochkina <tatianakurochkina96@mail.ru>
2019-10-15 02:33:38 +0400

    Fix only keyboard using outline

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV === 'development') {
   require('file-loader!./index.pug');
 }
 
+import './scripts/focus';
 import './scripts/scroll';
 import './scripts/menu';
 import './scripts/skills';
