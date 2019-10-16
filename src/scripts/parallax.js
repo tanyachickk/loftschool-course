d522edf006e06e255f4306633ae164866c454591
Tatiana Kurochkina <tatianakurochkina96@mail.ru>
2019-10-17 00:04:28 +0400

    Add leaves parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,9 +1,12 @@
 import constants from '../styles/variables.json';
 
-const parallax = document.querySelector('.parallax');
-const layers = parallax.children;
+const mainParallax = document.querySelector('.parallax');
+const mainLayers = mainParallax.children;
 
-function moveLayersDependsOnScroll(wScroll) {
+const leavesParallax = document.querySelector('.leaves-parallax');
+const leaves = leavesParallax.children;
+
+function moveLayersDependsOnScroll(layers, wScroll) {
   Array.from(layers).forEach((layer) => {
     const divider = layer.dataset.speed;
     const offset = -wScroll / divider;
@@ -12,8 +15,25 @@ function moveLayersDependsOnScroll(wScroll) {
 }
 
 window.addEventListener('scroll', () => {
-  if (window.innerWidth > parseInt(constants['bp-tablets']) + 1) {
-    const wScroll = window.pageYOffset;
-    moveLayersDependsOnScroll(wScroll);
+  if (window.innerWidth < parseInt(constants['bp-tablets'])) {
+    return;
+  }
+  const wScroll = window.pageYOffset;
+
+  const isMainParallax = window.innerHeight > wScroll;
+  if (isMainParallax) {
+    console.log('isMainParallax');
+    moveLayersDependsOnScroll(mainLayers, wScroll);
+    return;
+  }
+
+  const { top } = leavesParallax.getBoundingClientRect();
+  const leavesParallaxOffset = window.innerHeight - top;
+  const isLeavesParallax = leavesParallaxOffset > 0;
+  if (isLeavesParallax) {
+    console.log('isLeavesParallax', wScroll, top);
+
+    moveLayersDependsOnScroll(leaves, leavesParallaxOffset);
+    return;
   }
 });
