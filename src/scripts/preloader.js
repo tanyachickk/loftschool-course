23535dd3a6cc7f44d807f7a812cd33b62007f47a
Tatiana Kurochkina <tatianakurochkina96@mail.ru>
2019-11-11 22:11:48 +0400

    Fix preloader

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -1,6 +1,19 @@
 const preloader = document.querySelector('.preloader');
+const images = document.querySelectorAll('.parallax__image');
 
-window.addEventListener("load", () => {  
+const promises = [...images].map((image) => loadImage(image.currentSrc));
+Promise.all(promises).then(() => {
   preloader.style.display = 'none';
   document.body.style.overflow = 'auto';
 });
+
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => resolve(img));
+    img.addEventListener('error', () => {
+      reject(new Error(`Failed to load image's URL: ${url}`));
+    });
+    img.src = url;
+  });
+}
